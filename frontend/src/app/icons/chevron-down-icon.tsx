8d532728f8df22b2f0c8216ff18142be93486410
Matthew MacLennan <maclennan.mah@gmail.com>
2025-06-01 14:41:58 -0400

    fix: update icon properties

--- frontend/src/app/icons/chevron-down-icon.tsx
+++ frontend/src/app/icons/chevron-down-icon.tsx
@@ -5,7 +5,7 @@ interface ChevronDownIconProps extends React.ComponentPropsWithoutRef<'svg'> {
 export function ChevronDownIcon({ size, style, ...others }: ChevronDownIconProps) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
              className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M6 9l6 6l6 -6"/>
