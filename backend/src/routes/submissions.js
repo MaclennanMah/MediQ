8bf65b5aacba1a7f297704949270d0465c76a069
hamzateli <hteli1@myseneca.ca>
2025-06-10 19:51:08 -0400

    Added a route that returns all submissions submitted by patients

--- backend/src/routes/submissions.js
+++ backend/src/routes/submissions.js
@@ -18,4 +18,19 @@ router.get('/', async (req, res) => {
     }
 })
 
+/**
+ * GET /submissions
+ * --> This route returns all wait time submissions in the Wait Time Submission collection in mongodb
+ */
+router.get('/patient', async (req, res) => {
+    try {
+        const allWaitTimeSubmissions = await WaitTimeSubmission.find({submittedBy: 'patient'})
+        .lean();
+        res.json(allWaitTimeSubmissions)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: 'Could not fetch wait time submissions'})
+    }
+})
+
 export default router;
