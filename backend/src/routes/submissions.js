defbb1f6e53099c6293875c5b6da57a3dc8eb8db
hamzateli <hteli1@myseneca.ca>
2025-06-10 19:47:02 -0400

    Added route that returns all wait time submissions

---
+++ backend/src/routes/submissions.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import WaitTimeSubmission from '../../database/mongodb_db/models/WaitTimeSubmission.js';
+
+// Set router
+const router = express.Router();
+
+/**
+ * GET /submissions
+ * --> This route returns all wait time submissions in the Wait Time Submission collection in mongodb
+ */
+router.get('/', async (req, res) => {
+    try {
+        const allWaitTimeSubmissions = await WaitTimeSubmission.find().lean();
+        res.json(allWaitTimeSubmissions)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: 'Could not fetch wait time submissions'})
+    }
+})
+
+export default router;
