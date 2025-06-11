5bbacb775441900ddc6c26abcbdc7dd2099b9415
hamzateli <hteli1@myseneca.ca>
2025-06-10 20:18:44 -0400

    Added the route /submissions/latest/patient/:orgID which returns the latest patient submitted wait time for a given organization. Also added extra validation that handles an incorrect organization ID input

--- backend/src/routes/submissions.js
+++ backend/src/routes/submissions.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import WaitTimeSubmission from '../../database/mongodb_db/models/WaitTimeSubmission.js';
 
 // Set router
@@ -54,13 +55,57 @@ router.get('/organization', async (req, res) => {
  *     a specific organization the Wait Time Submission collection in mongodb
  */
 router.get('/latest/organization/:orgID', async (req, res) => {
+    // Get the org ID
+    const {orgID} = req.params;
+
+    // First check if the org id is valid
+    if (!mongoose.Types.ObjectId.isValid(orgID)) {
+        return res.status(400).json({error: 'Error! An invalid organization id was entered'})
+    }
+    
     try {
-        const {orgID} = req.params;
 
         const latestOrganizationWaitTimeSubmission = await WaitTimeSubmission
         .findOne({
             organizationId: orgID,
             submittedBy: 'organization'
+        })
+        .sort({submissionDate: -1})
+        .lean();
+
+
+        // If nothing was found
+        if (!latestOrganizationWaitTimeSubmission) {
+            return res.status(404).json({message: `The latest organization wait time submission for ${orgID} could not be found!`})
+        }
+
+        return res.json(latestOrganizationWaitTimeSubmission)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: 'Could not fetch wait time submissions'})
+    }
+})
+
+/**
+ * GET /submissions/latest/patient/orgID
+ * --> This route returns all organization wait time submissions for
+ *     a specific organization the Wait Time Submission collection in mongodb
+ */
+router.get('/latest/patient/:orgID', async (req, res) => {
+     // Get the org ID
+    const {orgID} = req.params;
+
+    // First check if the org id is valid
+    if (!mongoose.Types.ObjectId.isValid(orgID)) {
+        return res.status(400).json({error: 'Error! An invalid organization id was entered'})
+    }
+    
+    try {
+        
+        const latestOrganizationWaitTimeSubmission = await WaitTimeSubmission
+        .findOne({
+            organizationId: orgID,
+            submittedBy: 'patient'
         })
         .sort({submissionDate: -1})
         .lean();
